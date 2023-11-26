bf6a705d9ac38aa4cf45a66adf2f9e333a66cda0
Asish-Tammana <tammanaasish@gmail.com>
2023-11-26 22:38:52 +0530

    Code updated for landing page

--- src/Components/LandingPage/index.js
+++ src/Components/LandingPage/index.js
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom'
+import resume from '../../asserts/resume.pdf'
+
 
 const LandingPage = () => {
 
+
     return (
         <div>
-            <h1>Landing Page</h1>
+            <h1>Asish</h1>
+            <p>B.Tech - Computer Science and Engineering (May 2024)</p>
+            <a href={resume} download="Asish Tammana Resume.pdf">Download Resume</a>
+            <br/>
+            <br/>
+            <Link to="/bmi">
+                <button>Check BMI</button>
+            </Link>
         </div>
     )
 }
